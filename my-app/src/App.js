8131569903088727d53b4d69d13f88d19dcedfbf
jessfraser8 <jfraser4@usf.edu>
2024-07-15 16:00:06 -0400

    Implement RateMovieButton with user feedback.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+// U09226075
+// App.js
+
+import React, { useState } from 'react';
 import './App.css';
+
+
+function RateMovieButton() {
+  const [rating, setRating] = useState(null);
+  return(
+    <div className='rating'>
+      {[...Array(5)].map((star, index) => {
+        const currentRating = index + 1;
+        return(
+          <label>
+            <input class="fa fa-star fa-2x" type='radio' name='rating' value={currentRating} onClick={() => setRating(currentRating)}/>
+            {/* <span class="fa fa-star"></span> */}
+          </label>
+        )
+      })}
+      <p>Rating: {rating} stars</p>
+      
+    </div>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Rate the movie from 1 to 5 stars:</h3>
+      <RateMovieButton/>
     </div>
   );
 }
 
 export default App;
+
+
